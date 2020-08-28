600db61bc6deafffddb4f22c72bc320e33de62d8
Caleb Groff <cjgroff@email.arizona.edu>
2020-08-28 16:33:45 -0700

    Problem 2

--- practicum/week-1/question2.js
+++ practicum/week-1/question2.js
@@ -10,6 +10,21 @@ Example: input: cookies = [3, 5, 8] , extraCookies = 8
  */
 
  var canGetEqualCookies = function(cookies, extraCookies) {
-     
-    
+         let sum = 0
+         for (let i = 0; i < cookies.length; i += 1)
+         {
+                 sum += cookies[i]
+
+         }
+         sum += extraCookies
+         if (sum%cookies.length == 0)
+         {
+                 return true
+         }
+         else 
+         {
+                 return false 
+         }
+
+
  };
