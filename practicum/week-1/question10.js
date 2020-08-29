8d7d40abbd618aa37f0fa3783aa913862093ca53
Caleb Groff <cjgroff@email.arizona.edu>
2020-08-28 18:50:31 -0700

    Problem 10

--- practicum/week-1/question10.js
+++ practicum/week-1/question10.js
@@ -8,4 +8,12 @@ output: 7 - 2 = 5
 
 var maxMinusMin = function(n) {
 
+    d = (n+"").split("")
+    d.sort()
+    let dnum0 = parseInt (d[0])
+    let dnum1 = parseInt (d[d.length-1])
+    return dnum1 - dnum0
+
+
+
 };
