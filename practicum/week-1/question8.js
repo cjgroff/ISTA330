57a35b7b662a4d2297fce4ffb0efb00bb0c89171
Caleb Groff <cjgroff@email.arizona.edu>
2020-08-28 18:22:58 -0700

    Problem 8

--- practicum/week-1/question8.js
+++ practicum/week-1/question8.js
@@ -4,9 +4,44 @@ find out how many numbers in the array are both even and bigger than the number.
 
 Example:
 input: [23, 44, 12, 4]
-output: [1, 0, 1, 2]
+        0   1   2   3
+        i j
+        0 0 skip
+        0 1
+        0 2
+        0 3
+        1 0
+        1 2
+        1 3
+        2 0
+        2 1
+        2 3
+
+    
+
+output: [1,]
 */
 
 var biggerAndEven = function(input) {
+    let counts = []
+    
+    
+    for (let i = 0; i < input.length; i += 1)
+    {
+        let count = 0
+        for (let j = 0 ; j < input.length; j += 1){
+            if (i == j){
+                continue
+            }
+            if (input[j] > input[i] && input[j]%2 == 0){
+                count += 1
+                
+            }
 
+            
+        }
+        counts.push(count)
+    }
+    return counts
+  
 };
