824af302989c08afc10203346a24f375b31730ae
Caleb Groff <cjgroff@email.arizona.edu>
2020-08-28 17:03:32 -0700

    Problem 4

--- practicum/week-1/question4.js
+++ practicum/week-1/question4.js
@@ -8,8 +8,19 @@ The letters in A are guaranteed distinct, and all characters in A and B are lett
  Example:
  input: A='gtY', B = 'iTygrtg'
  output: 3
+ if B[i] is found in A then count
 */
 
 var howManyCommon = function(A, B) {
+    let count = 0
+    for (let i = 0; i < B.length; i += 1)
+    {
+        if (A.indexOf(B[i]) != -1 )
+        {
+            count += 1
+        }
+    }
+
+    return count
 
 };
