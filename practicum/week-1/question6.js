9db7bf100c743952fb84257fe12f3e5a5c4d396b
Caleb Groff <cjgroff@email.arizona.edu>
2020-08-28 17:50:40 -0700

    Problem 6

--- practicum/week-1/question6.js
+++ practicum/week-1/question6.js
@@ -2,9 +2,23 @@
 Given an array of non-negative integers, return  the median (https://en.wikipedia.org/wiki/Median) of the array. 
 Example: 
 input: [1, 2, 2, 3, 4, 7, 9]
+        0  1  2  3  4  5  6
 output: 3 
 */
 
 var median = function(input) {
+    let mid = Math.floor(input.length/2)
+    console.log('mid:', mid)
+    if (input.length%2 == 0)
+    {
+        
+        return (input[mid-1]+input[mid])/2
+    
+    }
+    else
+    {
+        
+        return input[mid]
 
+    }
 };
