5ce6c4cb36c1593ccad45c397c764de48198ba48
Caleb Groff <cjgroff@email.arizona.edu>
2020-09-03 16:12:06 -0700

    problem 7

--- practicum/week-1/question10.js
+++ practicum/week-1/question10.js
@@ -6,11 +6,27 @@ Example:
 input: 472
 output: 7 - 2 = 5
 */
-
+var sort = function(input){
+    for (let i = 0; i < input.length; i+= 1)
+    {
+        let minpos = i
+        for(let j = minpos; j < input.length; j += 1)
+        {
+            if(input[j]< input[minpos])
+            {
+                minpos = j
+            }
+        }
+        //At this minpos minpos hold the index of small value.
+        let temp = input[i]
+        input[i] = input[minpos]
+        input[minpos] = temp
+    }
+}
 var maxMinusMin = function(n) {
 
     d = (n+"").split("")
-    d.sort()
+    sort(d)
     let dnum0 = parseInt (d[0])
     let dnum1 = parseInt (d[d.length-1])
     return dnum1 - dnum0
