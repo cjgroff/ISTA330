d920a2cb618ab50d18a83877fe3eb065edce57f3
Caleb Groff <cjgroff@email.arizona.edu>
2020-09-05 15:56:10 -0700

    Problem 2.1

--- practicum/week-2/question1.js
+++ practicum/week-2/question1.js
@@ -8,7 +8,30 @@ is at least twice as much as every other number
          input: [3,2,21,50]
          output: true
 */
-
+var Maxnumber = function(input){
+        let l = input.length;
+        let max = -Infinity;
+        for (i = 0; l > i; i++) {
+            if (input[i] > max) {
+                max = input[i];
+            }
+        }
+        return max
+}
 var largestNumberIsAtLeastTwice = function(input) {
+        let max = Maxnumber(input)
+        let halfmax = max/2
+        for (let i = 0; i < input.length; i+= 1)
+        {
+                if(input[i] == max)
+                {
+                        continue
+                }
+                else if(input[i] > halfmax)
+                {
+                        return false
+                }
+        }
+        return true
    
 };
