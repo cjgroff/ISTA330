6aaa1866c9035e52a8e700a5d910df65132b266d
Caleb Groff <cjgroff@email.arizona.edu>
2020-09-05 17:21:32 -0700

    problem 2.5

--- practicum/week-2/question5.js
+++ practicum/week-2/question5.js
@@ -15,8 +15,31 @@ Example:
 input: [12, 6, 2, 2, 2, 0]
 output: true
 */
+//7,5,2,4
+// 1 1 -1
+// 7-5=1 sign  
+//7,7,5,5,3,3
+// 0 1 0 1  0
 
 var isMonotonic = function(input) {
+    let sign = undefined
+    for (let i = 0; i < input.length-1; i+= 1)
+    {
+        
+        let newsign = Math.sign(input[i]-input[i+1])
+        if(newsign == 0){
+            continue
+        }
+        else if(sign == undefined)
+        {
+            sign = newsign
+        }
+        else if(sign != newsign)
+        {
+            return false
+        }
+    }
+    return true
 
 };
 
